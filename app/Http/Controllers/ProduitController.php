d66418822c5dab61bc1c912cab084a98edfcd475
abdellah khouden <khoudenak@gmail.com>
2025-01-20 00:01:50 +0100

    create store function

--- app/Http/Controllers/ProduitController.php
+++ app/Http/Controllers/ProduitController.php
@@ -16,4 +16,23 @@ class ProduitController extends Controller
     {
         return view('produits.create');
     }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nom' => 'required',
+            'description' => 'required',
+            'prix' => 'required',
+            'quantite' => 'required',
+        ]);
+
+        $produit = new Produit();
+        $produit->nom = $request->nom;
+        $produit->description = $request->description;
+        $produit->prix = $request->prix;
+        $produit->quantite = $request->quantite;
+        $produit->save();
+
+        return redirect()->route('produits.index');
+    }
 }
